aa691198e6456a97da886aec99e136da56583d65
DopeRrrr <519124998@qq.com>
2021-03-21 22:41:01 +0800

    demo.1

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+a, b, c, d, e, f, g, h = range(8)
+
+A = [
+    [0, random.uniform(0.5, 0.8), random.uniform(0.5, 0.8), random.uniform(0.5, 0.8), 0, random.uniform(0.5, 0.8), 0,
+     0],
+    [random.uniform(0.5, 0.8), 0, random.uniform(0.5, 0.8), 0, 0, 0, 0, 0],
+    [random.uniform(0.5, 0.8), random.uniform(0.5, 0.8), 0, random.uniform(0.5, 0.8), 0, random.uniform(0.5, 0.8), 0,
+     0],
+    [random.uniform(0.5, 0.8), 0, random.uniform(0.5, 0.8), 0, random.uniform(0.5, 0.8), 0, random.uniform(0.5, 0.8),
+     random.uniform(0.5, 0.8)],
+    [0, 0, 0, random.uniform(0.5, 0.8), 0, random.uniform(0.5, 0.8), 0, 0],
+    [random.uniform(0.5, 0.8), 0, random.uniform(0.5, 0.8), 0, random.uniform(0.5, 0.8), 0, 0],
+    [0, 0, 0, random.uniform(0.5, 0.8), 0, 0, 0, random.uniform(0.5, 0.8)],
+    [0, 0, 0, random.uniform(0.5, 0.8), 0, 0, random.uniform(0.5, 0.8), 0]
+]
+
+N = 8
+
+Cn = [random.randint(10, 20) for _ in range(N)]
+
+no = 5
+CR = 0.7
+T_DE = 20
+gamma = 0.6
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def fitness_value(delta):
+    sum = 0
+    for i in range(N):
+        sum += delta[i] * Cn[i]
+    return sum
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('my miaomiao')
+class X:
+    delta = []
+    fit = 0
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def __init__(self):
+        self.delta = [random.uniform(0, 1) for _ in range(N)]
+        self.fit = fitness_value(self.delta)
+
+
+x_list = []
+for i in range(N):
+    p = X()
+    x_list.append(p)
