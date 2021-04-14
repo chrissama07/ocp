f7afc9bf5d71d151997e772d0a8cb37e111b6958
chrissama07 <1553801752@qq.com>
2021-04-15 00:14:17 +0800

    added hiv data preprocessing

---
+++ pathes.py
@@ -0,0 +1,7 @@
+iceland_output = 'networks/iceland/output/iceland.output.txt'
+
+hiv_cost = 'hiv.cost.txt'
+hiv_curingrate = 'hiv.curingrate.txt'
+hiv_data = 'hiv.data.txt'
+hiv_parameter = 'hiv.parameter.txt'
+hiv_output = 'output/hiv.output.txt'
