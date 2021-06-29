00965fec8c76c15e50a2e8c4fa991f3c1a4d4b66
DopeRrrr <519124998@qq.com>
2021-06-29 17:41:51 +0800

    fix all

--- draw/draw_bar/draw_bar_2.py
+++ draw/draw_bar/draw_bar_2.py
@@ -10,23 +10,52 @@ Y1 = [0, 0, 0, 0, 0]
 Y2 = [0, 0, 0, 0, 0]
 Y3 = [0, 0, 0, 0, 0]
 
+i=0
 with open(pathes.bundesliga_output) as f:
     lines = f.readlines()
-
-i = 2
+j = 2
 lines.pop(0)
 lines = np.array(lines)
 for line in lines:
-    numbers = [float(i) for i in line.split()]
-Y[0]=numbers[1]
-Y1[0]=numbers[2]
-npsa1=np.loadtxt(pathes.bundesliga_SA_output)
-npts1=np.loadtxt(pathes.bundesliga_TS_output)
-Y2[0]=npsa1[19]
-Y3[0]=npts1[20]
+    numbers = [float(j) for j in line.split()]
+Y[i]=numbers[1]
+Y1[i]=numbers[2]
+npsai=np.loadtxt(pathes.bundesliga_SA_output)
+nptsi=np.loadtxt(pathes.bundesliga_TS_output)
+Y2[i]=npsai[20]
+Y3[i]=nptsi[20]
 
-print(Y2[0])
-print(Y3[0])
+i=1
+with open(pathes.highland_output) as f:
+    lines = f.readlines()
+j = 2
+lines.pop(0)
+lines = np.array(lines)
+for line in lines:
+    numbers = [float(j) for j in line.split()]
+Y[i]=numbers[1]
+Y1[i]=numbers[2]
+npsai=np.loadtxt(pathes.highland_SA_output)
+nptsi=np.loadtxt(pathes.highland_TS_output)
+Y2[i]=npsai[20]
+Y3[i]=nptsi[20]
+print(Y2[i])
+
+i=2
+with open(pathes.highshcool_output) as f:
+    lines = f.readlines()
+j = 2
+lines.pop(0)
+lines = np.array(lines)
+for line in lines:
+    numbers = [float(j) for j in line.split()]
+Y[i]=numbers[1]
+Y1[i]=numbers[2]
+npsai=np.loadtxt(pathes.highshcool_SA_output)
+nptsi=np.loadtxt(pathes.highshcool_TS_output)
+Y2[i]=npsai[20]
+Y3[i]=nptsi[20]
+print(Y2[i])
 # for line in lines:
 #     numbers = [float(i) for i in line.split()]
 #     Y[i] = numbers[1]
@@ -40,9 +69,9 @@ width = total_width / n
 
 fig=plt.figure(figsize=(9, 7))
 
-plt.title('bundesliga', size=20)
+plt.title('Algorithm_Comparison_1', size=20)
 
-plt.ylim(60, int(max(Y2) + 1))
+# plt.ylim(80, int(max(Y2) + 1))
 # plt.yticks(range(int(min(Y1)), int(max(Y) + 3), 1))
 
 plt.bar(X, Y, width=width, label='DE', tick_label=name_list, fc='indigo')
