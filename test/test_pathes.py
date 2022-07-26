b594116f14313bba8ff4f34ae7418516bd09b16b
chrissama07 <1553801752@qq.com>
2022-07-26 22:24:27 +0800

    added comparison of DESA, AGDE and GSK on cec2017 in D10, D30, D50

---
+++ test/test_pathes.py
@@ -0,0 +1,11 @@
+path_D10 = r'D:\profile\codes\python_projects\ocp\test\output\D10_cpu'
+path_D10_csv = r'D:\profile\codes\python_projects\ocp\test\output\csv\D10'
+result_path_D10 = r'D:\profile\codes\python_projects\ocp\test\output\results\D10'
+
+path_D30 = r'D:\profile\codes\python_projects\ocp\test\output\D30_cpu'
+path_D30_csv = r'D:\profile\codes\python_projects\ocp\test\output\csv\D30'
+result_path_D30 = r'D:\profile\codes\python_projects\ocp\test\output\results\D30'
+
+path_D50 = r'D:\profile\codes\python_projects\ocp\test\output\D50'
+path_D50_csv = r'D:\profile\codes\python_projects\ocp\test\output\csv\D50'
+result_path_D50 = r'D:\profile\codes\python_projects\ocp\test\output\results\D50'
